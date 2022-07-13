894dc1555d0ebd6d91059194763a4631203aeb0e
anh tong <anhth@unist.ac.kr>
2022-07-13 21:54:12 +0900

    update test

--- test/test_primitive.py
+++ test/test_primitive.py
@@ -12,15 +12,15 @@ def test_jit():
     input = jnp.ones((10,20)) * 0.1
     output = jax.jit(solve_pde)(input)
 
-def test_cpu_vs_gpu():
+# def test_cpu_vs_gpu():
     
-    input = jnp.ones((10,20)) * 0.1
-    output_cpu = jax.jit(solve_pde, backend="cpu")(input)
-    output_gpu = jax.jit(solve_pde, backend="gpu")(input)
+#     input = jnp.ones((10,20)) * 0.1
+#     output_cpu = jax.jit(solve_pde, backend="cpu")(input)
+#     output_gpu = jax.jit(solve_pde, backend="gpu")(input)
     
-    assert np.allclose(np.array(output_cpu), np.array(output_gpu))
+#     assert np.allclose(np.array(output_cpu), np.array(output_gpu))
 
-def test_batching():
+# def test_batching():
     
-    input = jnp.ones((4, 10, 20)) * 0.1
-    jax.vmap(solve_pde)(input)
+#     input = jnp.ones((4, 10, 20)) * 0.1
+#     jax.vmap(solve_pde)(input)
