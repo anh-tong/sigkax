2eb8357e4bccc6b1c4b044c450590db2cae49863
anh tong <anhth@unist.ac.kr>
2022-07-25 22:55:08 +0900

    try again

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -2,7 +2,9 @@
 
 cd /github/workspace
 
-SIGKAX_CUDA=yes python3 -m pip install .
+export SIGKAX_CUDA=yes
+python3 setup.py build_ext --inplace
+python3 -m pip install .
 python3 -c 'import sigkax; print(sigkax.__version__)'
 python3 - c 'from sigkax import cpu_ops'
 python3 -c 'from sigkax import gpu_ops'
